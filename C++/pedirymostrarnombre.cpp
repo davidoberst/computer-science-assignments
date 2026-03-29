299c9f086ce632c86e03bf1e94a3b2b54d83c278
davidoberst <juandavidbustosperez82@gmail.com>
2026-03-28 19:01:17 -0500

    Pedir nombre y mostrarlo

---
+++ C++/pedirymostrarnombre.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+//std = estandar
+//cout = character output
+//cin = caracter input 
+// >> enviar hacia adentro (inpit)
+//<< enviar hacia afuera (output)
+ int main(){
+   std::string name; 
+   std::cout<<"[:] Escribe tu nombre : ";
+   std::cin >> name;
+   std::cout<< name;
+ return 0;
+}
