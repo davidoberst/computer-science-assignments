fe218c369075229aa7aaccce9ef9374a9b1d7dc6
davidoberst <juandavidbustosperez82@gmail.com>
2026-04-04 00:39:52 -0500

    multiplicar cpp

---
+++ C++/multiplicar.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main(){
+    //opcion1
+    int number1 = 2;
+    int number2 = 2;
+    int multiplicacion = number1 * number2;
+    std::cout<<multiplicacion;
+return 0;
+}
