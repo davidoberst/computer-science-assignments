dc3a9aa5db8ad26640fc52577a8218caf2a731fe
davidoberst <juandavidbustosperez82@gmail.com>
2026-04-04 01:01:40 -0500

    Area de rectangulo

---
+++ C++/area_rectangulo.cpp
@@ -0,0 +1,10 @@
+//Area Rectangulo = multiplica la medida de su base por la medida de su altura 
+#include <iostream>
+int main(){
+    int base;
+    int altura;
+    std::cout<<"Ingresa la base : ";std::cin>>base;
+    std::cout<<"Ingresa la altura : ";std::cin>>altura;
+    std::cout<<"el area del rectangulo es : " << base*altura;
+    return 0;
+}
