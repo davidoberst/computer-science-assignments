18baf8b6ff67f3ee78100fde8928e18bcdce3e18
davidoberst <juandavidbustosperez82@gmail.com>
2026-04-05 01:49:40 -0500

    area de un circulo

---
+++ C++/area_circulo.cpp
@@ -0,0 +1,10 @@
+//Area de un circulo = pi x radio al cuadrado
+
+#include <iostream>
+int main(){
+    float pi = 3.1416 ;
+    int radio;
+    std::cout<<"Ingresa el radio del circulo : ";std::cin>>radio;
+    std::cout<<"Resultado = "<<pi * radio * radio;
+    return 0;
+}
