c48cf5cf69ac7ea39b1d3459100e16ed065f5824
davidoberst <juandavidbustosperez82@gmail.com>
2026-04-17 19:33:17 -0500

    uso de namespace

---
+++ C++/namespace.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string nombre;
+    cout << "Ingrese su nombre : ";
+    cin >> nombre;
+    cout << "tu nombre es :"<<nombre;
+    return 0;
+}
