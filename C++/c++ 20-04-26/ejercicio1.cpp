70436fdd610cc6bc8ebd51ad5eb75d5b5697ec08
davidoberst <juandavidbustosperez82@gmail.com>
2026-04-20 18:52:53 -0500

    Enhance user input and output for age and height
    Added input for age and height, and output for both.

--- C++/c++ 20-04-26/ejercicio1.cpp
+++ C++/c++ 20-04-26/ejercicio1.cpp
@@ -1,18 +1,21 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
 int main() {
-    string p_nombre;
-    string s_nombre;
-    string p_apellido;
-    cout << "Nombre : ";
-    cin >> p_nombre;
-    cout << "Segundo nombre : ";
-    cin >> s_nombre;
-    cout << "Primer apellido : ";
-    cin >> p_apellido;
-    cout << "Su nombre es : " <<p_nombre<<" "<<s_nombre<<"" <<""<<""<<p_apellido;
-    
-    
-    return 0;
-}
+int edad;
+double estatura;
+string p_nombre;
+string s_nombre;
+string p_apellido;
+cout << "Nombre : ";
+cin >> p_nombre;
+cout << "Segundo nombre : ";
+cin >> s_nombre;
+cout << "Primer apellido : ";
+cin >> p_apellido;
+    cout << "Su nombre es : " <<p_nombre<<" "<<s_nombre<<"" <<" "<<" "<<p_apellido; 
+cout << "Ingrese su estatura : ";
+cin >> estatura;
+cout << "Su estatura es : " << estatura << "cm";
+cout << "Ingrese su edad : ";
+cin >> edad;
+cout << "su edad es : "<<edad;
