af572d10691ca5576e4c8ab7a31395bbad7ae0c3
davidoberst <juandavidbustosperez82@gmail.com>
2026-04-20 18:42:25 -0500

    Add files via upload

---
+++ C++/c++ 20-04-26/ejercicio1.cpp
@@ -0,0 +1,18 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    string p_nombre;
+    string s_nombre;
+    string p_apellido;
+    cout << "Nombre : ";
+    cin >> p_nombre;
+    cout << "Segundo nombre : ";
+    cin >> s_nombre;
+    cout << "Primer apellido : ";
+    cin >> p_apellido;
+    cout << "Su nombre es : " <<p_nombre<<" "<<s_nombre<<"" <<""<<""<<p_apellido;
+    
+    
+    return 0;
+}
