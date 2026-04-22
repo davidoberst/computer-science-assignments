351b5f99fea18d0085b151f8c1120e7ca3f30488
davidoberst <juandavidbustosperez82@gmail.com>
2026-04-21 19:20:55 -0500

    Replace height input with number comparison logic

--- C++/c++ 21-04-26/ejercicio1.cpp
+++ C++/c++ 21-04-26/ejercicio1.cpp
@@ -1,11 +1,15 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
 int main() {
-int estatura;
-cout << "Ingrese su estatura : ";
-cin >> estatura;
-
-    
+  int num;
+  cout << "ingrese numero :";
+  cin >> num;
+  if(num > 100){
+      cout << "mayor a 100";
+  }else if (num < 100){
+      cout << "menor a 100";
+  }else if(num == 100){
+      cout<<"igual a 100";
+  }
     return 0;
-}
+    }
