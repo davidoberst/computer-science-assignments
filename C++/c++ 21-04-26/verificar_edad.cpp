a3ad30b80f67366af71004e2a263cfaad644b0c6
davidoberst <juandavidbustosperez82@gmail.com>
2026-04-22 18:29:57 -0500

    Add verificar_edad.cpp to check age eligibility

---
+++ C++/c++ 21-04-26/verificar_edad.cpp
@@ -0,0 +1,15 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    int edad;
+    cout << "indique edad : ";
+    cin >> edad;
+    if(edad >= 18){
+        cout << "es mayor";
+    }else{
+        cout << "usted es menor";
+    }
+
+    return 0;
+}
