46280f8c7170b43d0abffd7ae67b22d573f243d3
davidoberst <juandavidbustosperez82@gmail.com>
2026-04-21 19:27:50 -0500

    Add program to check if a number is even or odd

---
+++ C++/c++ 21-04-26/ejercicio3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int num;
+  cout << "Ingresa numero : ";
+  cin >> num;
+  if(num % 2 == 0){
+     cout << "par"; 
+  }else{
+      cout << "impar";
+  }
+    return 0;
+    }
