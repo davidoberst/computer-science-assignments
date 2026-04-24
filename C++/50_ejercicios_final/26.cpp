ceed2685287c0c7e1d04feacc5fc898eb5ffbf00
davidoberst <juandavidbustosperez82@gmail.com>
2026-04-24 14:11:05 -0500

    Create zodiac sign program based on birth date
    Add a program to display zodiac signs based on birth month and day.

---
+++ C++/50_ejercicios_final/26.cpp
@@ -0,0 +1,16 @@
+//RUA que dado el mes muestre el signo sodiacal
+#include <iostream>
+#include <map>
+using namespace std;
+int main() {
+int mes;
+float dia;
+while(true){
+    cout<<"Dia de Nacimiento: ";
+    cin>>dia;
+    cout<<"Mes de Nacimiento: ";
+    cin>>mes;
+    
+} //end while
+  return 0;
+}
