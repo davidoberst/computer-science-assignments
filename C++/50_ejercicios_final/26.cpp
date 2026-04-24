3699890197a78a0cf59c9a8a6936d2dfd3343a9d
davidoberst <juandavidbustosperez82@gmail.com>
2026-04-24 14:25:06 -0500

    Change dia type from float to int and update input

--- C++/50_ejercicios_final/26.cpp
+++ C++/50_ejercicios_final/26.cpp
@@ -2,14 +2,22 @@
 #include <iostream>
 #include <map>
 using namespace std;
+
 int main() {
 int mes;
-float dia;
+int dia;
+string meses[] = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+
 while(true){
+    
+    //user inputs 
     cout<<"Dia de Nacimiento: ";
     cin>>dia;
     cout<<"Mes de Nacimiento: ";
     cin>>mes;
+    
+    //validacion de signo
+    
     
 } //end while
   return 0;
