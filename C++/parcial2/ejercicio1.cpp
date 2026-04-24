8bd650a7c291489b14962ed5ae62318a7b5c26a4
davidoberst <juandavidbustosperez82@gmail.com>
2026-04-23 19:42:27 -0500

    Add files via upload

---
+++ C++/parcial2/ejercicio1.cpp
@@ -0,0 +1,11 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+int estatura;
+cout << "Ingrese su estatura : ";
+cin >> estatura;
+
+    
+    return 0;
+}
