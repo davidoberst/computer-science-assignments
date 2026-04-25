ce78b7714e5c6e7367fb3b175c22d85b6a5d211e
davidoberst <juandavidbustosperez82@gmail.com>
2026-04-25 15:44:34 -0500

    Create 28.cpp

---
+++ C++/50_ejercicios_final/28.cpp
@@ -0,0 +1,13 @@
+//28. RUA que calcule la suma de los números que hay desde 1 hasta un número introducido por teclado.
+
+#include <iostream>
+using namespace std;
+int main() {
+    int user_num;
+    cout << "Ingrese su numero : ";
+    cin >> user_num;
+    for(int x=1; x<=user_num; x++){
+      cout<<x<<endl;   
+    }
+  return 0;
+}
