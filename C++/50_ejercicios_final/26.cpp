e985ab142b58c366188f2434357d4bbe1966e964
davidoberst <juandavidbustosperez82@gmail.com>
2026-04-25 15:10:28 -0500

    Update 26.cpp

--- C++/50_ejercicios_final/26.cpp
+++ C++/50_ejercicios_final/26.cpp
@@ -1,24 +1,25 @@
 //RUA que dado el mes muestre el signo sodiacal
 #include <iostream>
-#include <map>
 using namespace std;
+int dia;
+int mes;
+string validacion(){
+  //Aries : 21 de marzo – 19 de abril
+  if( (dia>=21 && mes == 3 && dia<=31 && mes == 3) || (dia>=19 && mes == 4 ) ){
+     return "Usted es Aries.";
+     cout<<endl;
+ }
+} //end validacion_function
 
 int main() {
-int mes;
-int dia;
-string meses[] = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
-
-while(true){
-    
+ while(true){
     //user inputs 
+    cout<<endl;
     cout<<"Dia de Nacimiento: ";
     cin>>dia;
     cout<<"Mes de Nacimiento: ";
     cin>>mes;
-    
-    //validacion de signo
-    
-    
+    cout << validacion();
 } //end while
   return 0;
 }
