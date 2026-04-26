75941bd254947f6d9d63d617d47700cf259f47a2
davidoberst <juandavidbustosperez82@gmail.com>
2026-04-26 15:33:57 -0500

    Add color survey functionality with counting

--- C++/50_ejercicios_final/40.cpp
+++ C++/50_ejercicios_final/40.cpp
@@ -3,25 +3,29 @@
 #include <iostream>
 #include <vector>
 #include <algorithm> //trnasform
+#include <map>
 using namespace std;
 int main() {
     vector<string> encuesta;
     string respuesta;
+    map<string, int> contador
+    contador["rojo"] = 0;
+    contador["verde"] = 0;
+    contador["azul"] = 0;
     for(int x = 1 ; x<=10; x++){
      cout << "Cual color le gusta mas entre (rojo, verde, azul)"<<"("<<x<<"/10) : ";
      cin >> respuesta;
      transform(respuesta.begin(), respuesta.end(), respuesta.begin(), ::tolower);
      if(respuesta == "rojo" || respuesta == "verde" || respuesta == "azul") {
        encuesta.push_back(respuesta); 
+       contador[respuesta]++;
      }else{
         cout<<"Respuesta fuera del rango."<<endl;
         abort();
      }
-    
     }
     for (auto x : encuesta) {
     cout << x << " ";
     }
     return 0;
-}n 0;
 }
