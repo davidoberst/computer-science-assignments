68ab7108aecb88dd089d88bde00ebe3da3a6febf
davidoberst <juandavidbustosperez82@gmail.com>
2026-04-27 18:33:44 -0500

    Add while loop example in while.cpp

---
+++ C++/while.cpp
@@ -0,0 +1,12 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    int x = 0;
+    while(x<=10){
+       cout <<x<<endl; 
+       x++;
+    }
+
+    return 0;
+}
