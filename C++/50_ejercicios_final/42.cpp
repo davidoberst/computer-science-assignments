b6d6456460595a258473bbf6ea2d9f53f4c13a0a
davidoberst <juandavidbustosperez82@gmail.com>
2026-04-28 18:51:09 -0500

    Implement vehicle counting program
    This program counts the number of cars, buses, and other vehicles that pass through a toll, and allows the user to exit by entering 'x'.

---
+++ C++/50_ejercicios_final/42.cpp
@@ -0,0 +1,30 @@
+//42. En un determinado peaje se desea saber cuántos carros particulares y cuántos buses pasaron
+//en un día, lo mismo que el promedio de personas que viajan en carro particular y el promedio
+//de personas que viajan en bus. Se debe tener en cuenta que por cada vehículo que pase, se
+//debe indagar por el tipo de vehículo que es y el número de pasajeros que transporta. 
+//carro = 1 ,  bus = 2, otro = 3
+
+#include <iostream>
+using namespace std;
+int main() {
+    //variables
+   string decision;
+   int carros = 0;
+   int buses = 0;
+   int otros = 0;
+   while(1){
+   cout<<"Ingrese un vehiculo (carro-bus-otro) ingrese x para salir : " ;
+   cin>>decision;
+   if(decision == "carro"){
+      carros +1;
+   }else if(decision == "bus"){
+      buses +1;
+    }else if(decision == "x" || decision == "X"){
+      break;   
+    }else{
+        otros+1;
+    }
+   }//end while
+  
+ return 0;
+}
