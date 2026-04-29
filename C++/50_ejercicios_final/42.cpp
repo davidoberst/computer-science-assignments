7471516f38089d77451f0d5eec6728b84e91aee3
davidoberst <juandavidbustosperez82@gmail.com>
2026-04-28 19:19:49 -0500

    Refactor vehicle counting program with person tracking

--- C++/50_ejercicios_final/42.cpp
+++ C++/50_ejercicios_final/42.cpp
@@ -6,25 +6,58 @@
 
 #include <iostream>
 using namespace std;
+
 int main() {
     //variables
-   string decision;
-   int carros = 0;
-   int buses = 0;
-   int otros = 0;
-   while(1){
-   cout<<"Ingrese un vehiculo (carro-bus-otro) ingrese x para salir : " ;
-   cin>>decision;
-   if(decision == "carro"){
-      carros +1;
-   }else if(decision == "bus"){
-      buses +1;
+    string decision;
+    int decision_personas;
+    int carros = 0;
+    int personas_carro = 0;
+    int buses = 0;
+    int personas_buses = 0;
+    int otros = 0;
+    int personas_otro = 0;
+
+    while(1){
+    cout<<"Ingrese un vehiculo (carro-bus-otro) ingrese x para salir : " ;
+    cin>>decision;
+
+    if(decision == "carro"){
+       carros += 1;
+       cout<<"Cuantas personas hay adentro? :";
+       cin >> decision_personas;
+       personas_carro += decision_personas;
+
+    }else if(decision == "bus"){
+       buses += 1;
+       cout<<"Cuantas personas hay adentro? :";
+       cin >> decision_personas;
+       personas_buses += decision_personas;
+
     }else if(decision == "x" || decision == "X"){
-      break;   
+       cout<<endl<<"--------RESULTADOS--------------"<<endl;
+       cout<<"Carros que pasaron : "<<carros<<endl;
+       cout<<"Buses que pasaron : "<<buses<<endl;
+       cout<<"Otros que pasaron : "<<otros<<endl;
+       cout<<endl<<"-------------------------"<<endl;
+       
+       // Promedios
+       if(carros > 0) {
+           cout << "Promedio de personas en carro: " << (float)personas_carro / carros << endl;
+       }
+       if(buses > 0) {
+           cout << "Promedio de personas en bus: " << (float)personas_buses / buses << endl;
+       }
+       
+       break;    
+       
     }else{
-        otros+1;
-    }
-   }//end while
-  
- return 0;
+        otros += 1;
+        cout<<"Cuantas personas hay adentro? :";
+        cin >> decision_personas;
+        personas_otro += decision_personas;
+     }
+    }//end while
+   
+    return 0;
 }
