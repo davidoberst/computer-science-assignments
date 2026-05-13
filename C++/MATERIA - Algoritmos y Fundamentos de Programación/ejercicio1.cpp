b6ed4536a3d88df167ba274be0293cbfe258f5c4
davidoberst <juandavidbustosperez82@gmail.com>
2026-05-12 19:33:52 -0500

    Add files via upload
    agregar nueva materia

---
+++ C++/MATERIA - Algoritmos y Fundamentos de Programación/ejercicio1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+int edad;
+double estatura;
+string p_nombre;
+string s_nombre;
+string p_apellido;
+cout << "Nombre : ";
+cin >> p_nombre;
+cout << "Segundo nombre : ";
+cin >> s_nombre;
+cout << "Primer apellido : ";
+cin >> p_apellido;
+    cout << "Su nombre es : " <<p_nombre<<" "<<s_nombre<<"" <<" "<<" "<<p_apellido; 
+cout << "Ingrese su estatura : ";
+cin >> estatura;
+cout << "Su estatura es : " << estatura << "cm";
+cout << "Ingrese su edad : ";
+cin >> edad;
+cout << "su edad es : "<<edad;
