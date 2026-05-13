0c99a23eb45a8b8e5fcffe378808b6c479738f66
davidoberst <juandavidbustosperez82@gmail.com>
2026-05-12 19:34:16 -0500

    Refactor ejercicio1.cpp for basic arithmetic operations

--- C++/MATERIA - Algoritmos y Fundamentos de Programación/ejercicio1.cpp
+++ C++/MATERIA - Algoritmos y Fundamentos de Programación/ejercicio1.cpp
@@ -1,21 +1,42 @@
+//realizar un aslgorimo donde usemos las 4 operaciones matematicas. +-*/ suma entero, resta tambien, multiplicaion
+//double, division float
 #include <iostream>
+#include <string>
 using namespace std;
 int main() {
-int edad;
-double estatura;
-string p_nombre;
-string s_nombre;
-string p_apellido;
-cout << "Nombre : ";
-cin >> p_nombre;
-cout << "Segundo nombre : ";
-cin >> s_nombre;
-cout << "Primer apellido : ";
-cin >> p_apellido;
-    cout << "Su nombre es : " <<p_nombre<<" "<<s_nombre<<"" <<" "<<" "<<p_apellido; 
-cout << "Ingrese su estatura : ";
-cin >> estatura;
-cout << "Su estatura es : " << estatura << "cm";
-cout << "Ingrese su edad : ";
-cin >> edad;
-cout << "su edad es : "<<edad;
+ int n1 , n2 ;  
+ double n3, n4 ;
+ float n5 , n6 ;
+ 
+ //SUMA   
+ cout<<"SUMA : "<<endl;
+ cout<<"ingresa numero 1 : "<<endl;
+ cin>>n1;
+ cout<<"ingresa numero 2  : "<<endl;
+ cin >> n2;
+ cout<<"RESULTADO : "<<n1+n1<<endl;
+ 
+ //RESTA
+ cout<<"RESTA : "<<endl;
+ cout<<"ingresa numero 1 : "<<endl;
+ cin>>n1;
+ cout<<"ingresa numero 2  : "<<endl;
+ cin >>n2;
+ cout<<"RESULTADO : "<<n1-n1<<endl;
+ 
+ //MULTIPLICACION
+ cout<<"MULTIPLICACION : "<<endl;
+ cout<<"ingresa numero 1 : "<<endl;
+ cin>>n3;
+ cout<<"ingresa numero 2  : "<<endl;
+ cin >>n4;
+ cout<<"RESULTADO : "<<n3 * n4<<endl;
+ 
+ // DIVISION 
+ cout<<"DIVISION : "<<endl;
+ cout<<"ingresa numero 1 : "<<endl;
+ cin>>n5;
+ cout<<"ingresa numero 2  : "<<endl;
+ cin >>n6;
+ cout<<"RESULTADO : "<<n5 / n6<<endl;
+}
