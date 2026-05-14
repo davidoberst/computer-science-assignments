4a36961d4b447fc76a4d656e1b0ad83e21824dca
davidoberst <juandavidbustosperez82@gmail.com>
2026-05-13 19:23:36 -0500

    Fix conditional checks for grading system

--- C++/MATERIA - Algoritmos y Fundamentos de Programación/ejercicio2.cpp
+++ C++/MATERIA - Algoritmos y Fundamentos de Programación/ejercicio2.cpp
@@ -1,3 +1,4 @@
+
 //imprima un numero y diga si es mayor menor o igual a 0
 
 #include <iostream>
@@ -8,14 +9,14 @@ int main(){
     cin >> calificacion;
     if(calificacion < 6){
         cout << "reprobado";
-    }else if(calificacion <=6 || calificacion <= 7){
-        cout << "suficiente";
-    }else if(calificacion <=7 || calificacion <= 8){
-        cout << "bien";
-    }else if(calificacion <=8 || calificacion <= 9 ){
-        cout << "muy bien";
-    }else if(calificacion <= 9 || calificacion <=10){
-        cout << "excelente";
+     }else if(calificacion <=6 || calificacion <= 7){
+         cout << "suficiente";
+      }else if(calificacion <=7 || calificacion <= 8){
+          cout << "bien";
+       }else if(calificacion <=8 || calificacion <= 9 ){
+           cout << "muy bien";
+         }else if(calificacion <= 9 || calificacion <=10){
+            cout << "excelente";
     }
     return 0;
 }
