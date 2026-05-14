49afec78a435e6a7d4baacfdc166661fc7810c5c
davidoberst <juandavidbustosperez82@gmail.com>
2026-05-13 19:07:59 -0500

    Add grading program based on user input

---
+++ C++/MATERIA - Algoritmos y Fundamentos de Programación/ejercicio2.cpp
@@ -0,0 +1,21 @@
+//imprima un numero y diga si es mayor menor o igual a 0
+
+#include <iostream>
+using namespace std;
+int main(){
+    int calificacion ;
+    cout<<"Ingrese su calificacion : ";
+    cin >> calificacion;
+    if(calificacion < 6){
+        cout << "reprobado";
+    }else if(calificacion <=6 || calificacion <= 7){
+        cout << "suficiente";
+    }else if(calificacion <=7 || calificacion <= 8){
+        cout << "bien";
+    }else if(calificacion <=8 || calificacion <= 9 ){
+        cout << "muy bien";
+    }else if(calificacion <= 9 || calificacion <=10){
+        cout << "excelente";
+    }
+    return 0;
+}
