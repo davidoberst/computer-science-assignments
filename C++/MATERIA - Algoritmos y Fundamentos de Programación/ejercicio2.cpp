205244d42e81d8756302e84e687d2cd48dba224a
davidoberst <juandavidbustosperez82@gmail.com>
2026-05-13 19:27:50 -0500

    Fix conditional checks for grading thresholds

--- C++/MATERIA - Algoritmos y Fundamentos de Programación/ejercicio2.cpp
+++ C++/MATERIA - Algoritmos y Fundamentos de Programación/ejercicio2.cpp
@@ -9,11 +9,11 @@ int main(){
     cin >> calificacion;
     if(calificacion < 6){
         cout << "reprobado";
-     }else if(calificacion <=6 || calificacion <= 7){
+     }else if(calificacion <=6 || calificacion < 7){
          cout << "suficiente";
-      }else if(calificacion <=7 || calificacion <= 8){
+      }else if(calificacion <=7 || calificacion < 8){
           cout << "bien";
-       }else if(calificacion <=8 || calificacion <= 9 ){
+       }else if(calificacion <=8 || calificacion < 9 ){
            cout << "muy bien";
          }else if(calificacion <= 9 || calificacion <=10){
             cout << "excelente";
